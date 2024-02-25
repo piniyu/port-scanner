806e0d031fa9462a944856e2d72bf83845ad8624
Ching Chen <piniyu699@gmail.com>
2024-02-25 18:37:47 +0800

    feat:use multi threads to scan over 60000 or so ports

--- src/main/java/PortScanner.java
+++ src/main/java/PortScanner.java
@@ -1,38 +1,82 @@
 import org.apache.commons.cli.*;
 
-import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.Socket;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class PortScanner {
+    private static final int MAX_THREADS = 100;
+    private static final int TIMEOUT = 100;
+
     public static void main(String[] args) {
-        Options options = new Options();
-        options.addOption("h", "host", true, "Target host");
-        options.addOption("p", "port", true, "Target port");
+        try (ExecutorService executor = Executors.newFixedThreadPool(MAX_THREADS)) {
 
-        CommandLineParser parser = new DefaultParser();
-        try {
-            CommandLine cmd = parser.parse(options, args);
+            Options options = new Options();
+            options.addOption("h", "host", true, "Target host");
+            options.addOption("p", "port", true, "Target port");
 
-            String host = cmd.getOptionValue("host");
-            int port = Integer.parseInt(cmd.getOptionValue("port"));
+            CommandLineParser parser = new DefaultParser();
+            try {
+                CommandLine cmd = parser.parse(options, args);
 
-            if (isPortOpen(host, port)) {
-                System.out.println("Port: " + port + " is open");
+                String host = cmd.getOptionValue("host");
+
+                if (cmd.getOptionValue("port") == null) {
+                    Instant startInstance = Instant.now();
+                    for (int port = 1; port <= 65535; port++) {
+                        executor.submit(new PortScanTask(host, port));
+                    }
+                    executor.shutdown();
+
+                    try {
+                        executor.awaitTermination(Long.MAX_VALUE, TimeUnit.MILLISECONDS);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println(Duration.between(startInstance, Instant.now()));
+                } else {
+                    int port = Integer.parseInt(cmd.getOptionValue("port"));
+
+                    if (isPortOpen(host, port)) {
+                        printOpenedPort(port);
+                    }
+                }
+
+
+            } catch (ParseException e) {
+                System.err.println(e.getMessage());
             }
-        } catch (ParseException e) {
-            System.err.println(e.getMessage());
+        }
+    }
+
+    private record PortScanTask(String host, int port) implements Runnable {
+
+        @Override
+        public void run() {
+            if (isPortOpen(host, port)) {
+                printOpenedPort(port);
+            } else {
+                System.out.println("Port " + port + " is closed");
+            }
         }
     }
 
     private static boolean isPortOpen(String host, int port) {
         try (Socket socket = new Socket()) {
             System.out.println("Scanning host: " + host + " port: " + port);
-            socket.connect(new InetSocketAddress(host, port), 1000);
+            socket.connect(new InetSocketAddress(host, port), TIMEOUT);
             System.out.flush();
             return true;
         } catch (Exception e) {
             return false;
         }
+    }
+
+    private static void printOpenedPort(int port) {
+        System.out.println("Port: " + port + " is open");
     }
 }
